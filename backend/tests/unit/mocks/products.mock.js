c90501863a161554f19f2eeace9caf1b0c8fc4d8
Luciano Prado <lucianobatuta@live.com>
2024-03-26 16:04:24 -0300

    Fixes Lint errors and adds folder and mock files to the products route

---
+++ backend/tests/unit/mocks/products.mock.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+];
+
+const product = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+];
+
+const createProduct = [
+  {
+    id: 4,
+    name: 'ProdutoX',
+  },
+];
+
+module.exports = {
+  products,
+  product,
+  createProduct,
+};
