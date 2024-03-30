350d4c55a0ad8d795bccc4264fd5f6aa5d835aac
Luciano Prado <lucianobatuta@live.com>
2024-03-30 11:19:49 -0300

    Create sales.mock file for sales route tests

---
+++ backend/tests/unit/mocks/sales.mock.js
@@ -0,0 +1,32 @@
+const salesDB = [
+  {
+    saleId: 1,
+    date: '2024-02-23T14:48:05.000Z',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: '2024-02-23T14:48:05.000Z',
+    productId: 2,
+    quantity: 10,
+  },
+];
+
+const salesById = [
+  {
+    date: '2021-09-09T04:54:29.000Z',
+    productId: 1,
+    quantity: 2,
+  },
+  {
+    date: '2021-09-09T04:54:54.000Z',
+    productId: 2,
+    quantity: 2,
+  },
+];
+
+module.exports = {
+  salesDB,
+  salesById,
+};
