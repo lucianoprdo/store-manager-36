c0c2c00ba3ab6e4d34942faf56478e7465f7e2a1
Luciano Prado <lucianobatuta@live.com>
2024-04-03 11:23:44 -0300

    Review testsmocks

--- backend/tests/unit/mocks/sales.mock.js
+++ backend/tests/unit/mocks/sales.mock.js
@@ -1,13 +1,13 @@
 const salesDB = [
   {
     saleId: 1,
-    date: '2021-09-09T04:54:29.000Z',
+    date: '2024-02-23T14:48:05.000Z',
     productId: 1,
     quantity: 5,
   },
   {
     saleId: 1,
-    date: '2021-09-09T04:54:54.000Z',
+    date: '2024-02-23T14:48:05.000Z',
     productId: 2,
     quantity: 10,
   },
